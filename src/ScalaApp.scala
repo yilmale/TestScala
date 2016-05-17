7a9bb08a74fa6da4d16ebe9cab29e3036f7b3bf1
yilmaz <yilmaz@auburn.edu>
2016-05-17 17:11:06 -0500

    Created Simulator, Activity, Scheduler, and Model components

--- src/ScalaApp.scala
+++ src/ScalaApp.scala
@@ -8,7 +8,7 @@ object ScalaApp {
 
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
-    
+
     Simulator setUp {
       new ActivityModel ("M1") {
         Activity (this) (name="A") (cond=true) {}
