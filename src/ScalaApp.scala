e65937c7178acae74b8734e1951cc626bdb37c0b
yilmaz <yilmaz@auburn.edu>
2016-05-31 13:07:09 -0500

    Updated ScalaApp

--- src/ScalaApp.scala
+++ src/ScalaApp.scala
@@ -9,38 +9,13 @@ object ScalaApp {
 
   def main(args: Array[String]): Unit = {
 
-    var dm = new DataModel {
-      val page =
-        <html>
-          <head>
-            <title>Hello XHTML world</title>
-          </head>
-          <body>
-            <h1>Hello world</h1>
-            <p><a href="scala-lang.org">Scala</a> talks XHTML</p>
-          </body>
-        </html>
-      println(page.toString())
-    }
 
-    var cm = new CognitiveModel {
-
-    }
-
-    var hm = new HypothesisModel {
-
-    }
-
-    var em = new ExperimentModel {
-
-    }
 
     var sim = new Simulator
     sim.setUp  {
       new ActivityModel("M1")  {
         Activity(name = "A1")(cond = true)(() => {
           println("test")
-          for (a <- dm.page.child) {println(a.mkString)}
         }) registerWith sim.simScheduler
         Activity(name = "A2")(cond = true)(() => {
           println("test1")
